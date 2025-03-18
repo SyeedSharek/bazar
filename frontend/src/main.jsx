70c0ce42116c307aeaf869ca170a25121020e4e7
SyeedSharek <syeedsharek@example.com>
2025-03-19 00:17:54 +0600

    noting

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import "./index.css";
 import "./App.css";
+
 import { Router } from "./router/router";
 
 const root = document.getElementById("root");
