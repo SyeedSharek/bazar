bae63366ba4c07998edb39dc2d860032af69c21d
SyeedSharek <syeedsharek@example.com>
2025-03-19 14:21:07 +0600

    navbar

---
+++ frontend/src/components/ui/icons/LightIcon.jsx
@@ -0,0 +1,8 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMoon } from '@fortawesome/free-regular-svg-icons';
+
+import React from 'react';
+
+export default function LightIcon() {
+  return <FontAwesomeIcon icon={faMoon} />;
+}
--- frontend/src/pages/backend/auth/dashboard/dashboard.jsx
+++ frontend/src/pages/backend/auth/dashboard/dashboard.jsx
@@ -3,5 +3,7 @@ import React from 'react'
 export default function Dashboard() {
   return (
     <div>dashboard</div>
+    
+    
   )
 }
